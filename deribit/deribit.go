6a104815ccb86bdc9cf2b5c1bacb1e3b7e7add69
Steven Wilkin <stevenwilkin@gmail.com>
2020-08-24 23:10:49 +0700

    Move structs into separate file

--- deribit/deribit.go
+++ deribit/deribit.go
@@ -13,32 +13,6 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-type authResponse struct {
-	Result struct {
-		AccessToken string `json:"access_token"`
-	} `json:"result"`
-}
-
-type requestMessage struct {
-	Method string                 `json:"method"`
-	Params map[string]interface{} `json:"params"`
-}
-
-type portfolioResponse struct {
-	Method string `json:"method"`
-	Params struct {
-		Data struct {
-			Equity float64 `json:"equity"`
-		} `json:"data"`
-	} `json:"params"`
-}
-
-type positionsResponse struct {
-	Result []struct {
-		Size float64 `json:"size"`
-	} `json:"result"`
-}
-
 type Deribit struct {
 	ApiId        string
 	ApiSecret    string
---
+++ deribit/structs.go
@@ -0,0 +1,27 @@
+package deribit
+
+type authResponse struct {
+	Result struct {
+		AccessToken string `json:"access_token"`
+	} `json:"result"`
+}
+
+type requestMessage struct {
+	Method string                 `json:"method"`
+	Params map[string]interface{} `json:"params"`
+}
+
+type portfolioResponse struct {
+	Method string `json:"method"`
+	Params struct {
+		Data struct {
+			Equity float64 `json:"equity"`
+		} `json:"data"`
+	} `json:"params"`
+}
+
+type positionsResponse struct {
+	Result []struct {
+		Size float64 `json:"size"`
+	} `json:"result"`
+}
