824d1f926536e17df4bbe320d4a7a81be8454869
Steven Wilkin <stevenwilkin@gmail.com>
2020-12-31 20:28:56 +0700

    Make binary executable after download

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,5 +3,6 @@
 set -eux
 
 curl -LOs https://github.com/stevenwilkin/treasury/releases/download/latest/treasuryd
+chmod a+x treasuryd
 sudo mv treasuryd /usr/local/bin
 sudo systemctl restart treasuryd
