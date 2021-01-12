867a7b7efa7c666dffafbb3522ee3e28023abcd4
Steven Wilkin <stevenwilkin@gmail.com>
2021-01-12 14:55:13 +0700

    Return BTC and USDT balances from Binance

--- binance/main.go
+++ binance/main.go
@@ -1,9 +1,16 @@
 package binance
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
 	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"net/url"
 	"strconv"
+	"time"
 
 	"github.com/gorilla/websocket"
 	log "github.com/sirupsen/logrus"
@@ -29,6 +36,67 @@ func (b *Binance) wsHostname() string {
 	} else {
 		return "stream.binance.com:9443"
 	}
+}
+
+func (b *Binance) sign(s string) string {
+	h := hmac.New(sha256.New, []byte(b.ApiSecret))
+	io.WriteString(h, string(s))
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func (b *Binance) GetBalances() (float64, float64, error) {
+	timestamp := time.Now().UnixNano() / int64(time.Millisecond)
+
+	params := fmt.Sprintf("timestamp=%d", timestamp)
+	signedParams := fmt.Sprintf("%s&signature=%s", params, b.sign(params))
+
+	u := url.URL{
+		Scheme:   "https",
+		Host:     b.hostname(),
+		Path:     "/api/v3/account",
+		RawQuery: signedParams}
+
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	req.Header.Set("X-MBX-APIKEY", b.ApiKey)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, 0, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	var response accountResponse
+	json.Unmarshal(body, &response)
+
+	var btc, usdt float64
+
+	for _, asset := range response.Balances {
+		if asset.Asset != "BTC" && asset.Asset != "USDT" {
+			continue
+		}
+
+		free, _ := strconv.ParseFloat(asset.Free, 64)
+		locked, _ := strconv.ParseFloat(asset.Locked, 64)
+		total := free + locked
+
+		if asset.Asset == "BTC" {
+			btc = total
+		} else {
+			usdt = total
+		}
+	}
+
+	return btc, usdt, nil
 }
 
 func (b *Binance) subscribeToPrice() *websocket.Conn {
--- binance/structs.go
+++ binance/structs.go
@@ -3,3 +3,11 @@ package binance
 type tickerMessage struct {
 	P string
 }
+
+type accountResponse struct {
+	Balances []struct {
+		Asset  string `json:"asset"`
+		Free   string `json:"free"`
+		Locked string `json:"locked"`
+	} `json:"balances"`
+}
