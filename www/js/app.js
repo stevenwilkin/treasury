c464f8c187b451d53ca12baad510e78e94f6d638
Steven Wilkin <stevenwilkin@gmail.com>
2021-05-25 22:19:23 +0700

    Display additional data in web UI

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,6 @@
+var normalColour = 'rgb(178, 178, 178)',
+    flashColour = '#FFF';
+
 function formatPrice(f) {
   return f.toFixed(2);
 }
@@ -15,9 +18,6 @@ function initPrices(prices) {
 }
 
 function updatePrices(prices) {
-  var normalColour = 'rgb(178, 178, 178)',
-      flashColour = '#FFF';
-
   for(var price in prices) {
     var item = $('.price-' + price),
         value = formatPrice(prices[price]);
@@ -41,6 +41,85 @@ function handlePrices(prices) {
     updatePrices(prices);
   } else {
     initPrices(prices);
+  }
+}
+
+function initAssets(assets) {
+  var templateVenue = $('#template-venue').html(),
+      templateAsset = $('#template-asset').html();
+
+  for(var venue in assets) {
+    var $venue = $(templateVenue.replace(/__NAME__/, venue)),
+        container = $venue.find('.venue-assets');
+
+    for(var asset in assets[venue]) {
+      var item = templateAsset
+                   .replace(/__NAME__/, asset)
+                   .replace(/__ID__/, 'asset-' + venue + '-' + asset)
+                   .replace(/__QUANTITY__/, assets[venue][asset]);
+      container.append(item);
+    }
+
+    $('.assets').append($venue);
+  }
+}
+
+function updateAssets(assets) {
+  for(var venue in assets) {
+    for(var asset in assets[venue]) {
+      var item = $('.asset-' +  + venue + '-' + asset),
+          value = assets[venue][asset];
+
+      if(item.text() == value) {
+        continue;
+      }
+
+      item
+        .text(value)
+        .css('color', flashColour);
+
+      setTimeout(function() {
+        item.css('color', normalColour);
+      }, 1000);
+    }
+  }
+}
+
+function handleAssets(assets) {
+  if($('.assets').children().length) {
+    updateAssets(assets);
+  } else {
+    initAssets(assets);
+  }
+}
+
+function handlePayload(json) {
+  if(json.assets) {
+    handleAssets(json.assets);
+  }
+
+  if(json.prices) {
+    handlePrices(json.prices);
+  }
+
+  var stats = ['exposure', 'cost', 'value', 'pnl', 'pnl_percentage'];
+  for(var i in stats) {
+    var stat = stats[i];
+
+    if(!json.hasOwnProperty(stat)) {
+      continue;
+    }
+
+    var value = json[stat];
+    if(stat == 'exposure') {
+      value = value.toFixed(8);
+    } else if(['cost', 'value', 'pnl'].includes(stat)) {
+      value = value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+    } else if(stat == 'pnl_percentage') {
+      value = value.toFixed(2) + '%';
+    }
+
+    $('.stat-' + stat).text(value);
   }
 }
 
@@ -72,8 +151,6 @@ $(function() {
       return;
     }
 
-    if(json.prices) {
-      handlePrices(json.prices);
-    }
+    handlePayload(json);
   }
 });
