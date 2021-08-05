8c920c5d66a33ac94d1a8029bde487212c5cd811
Steven Wilkin <stevenwilkin@gmail.com>
2021-08-05 16:52:00 +0700

    Disable alerter by default

---
+++ cmd/treasuryd/alerter.disabled.go
@@ -0,0 +1,5 @@
+// +build !alerter
+
+package main
+
+func initAlerter() {}
---
+++ cmd/treasuryd/alerter.go
@@ -0,0 +1,40 @@
+// +build alerter
+
+package main
+
+import (
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/stevenwilkin/treasury/alert"
+	"github.com/stevenwilkin/treasury/telegram"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func initAlerter() {
+	log.Info("Initialising alerter")
+
+	chatId, err := strconv.Atoi(os.Getenv("TELEGRAM_CHAT_ID"))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	notifier := &telegram.Telegram{
+		ApiToken: os.Getenv("TELEGRAM_API_TOKEN"),
+		ChatId:   chatId}
+
+	alerter = alert.NewAlerter(statum, notifier)
+	alerter.Retrieve()
+
+	ticker := time.NewTicker(1 * time.Second)
+	go func() {
+		for {
+			<-ticker.C
+			log.Debug("Checking alerts")
+			alerter.CheckAlerts()
+			alerter.Persist()
+		}
+	}()
+}
