83aa8a0f2080b37d96c57e2f0104dfba36ee18be
Steven Wilkin <stevenwilkin@gmail.com>
2023-06-02 21:09:12 +0700

    Use v5 API for remaining Bybit requests

--- bybit/bybit.go
+++ bybit/bybit.go
@@ -10,7 +10,6 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"sort"
 	"strconv"
 	"time"
 
@@ -35,30 +34,9 @@ func (b *Bybit) timestamp() string {
 	return strconv.FormatInt((time.Now().UnixNano() / int64(time.Millisecond)), 10)
 }
 
-func (b *Bybit) signedUrl(path string, addParams map[string]string) string {
-	params := map[string]interface{}{
-		"api_key":   b.ApiKey,
-		"timestamp": b.timestamp()}
-
-	for k, v := range addParams {
-		params[k] = v
-	}
-
-	keys := make([]string, len(params))
-	i := 0
-	query := ""
-	for k, _ := range params {
-		keys[i] = k
-		i++
-	}
-	sort.Strings(keys)
-	for _, k := range keys {
-		query += fmt.Sprintf("%s=%v&", k, params[k])
-	}
-	query = query[0 : len(query)-1]
-	h := hmac.New(sha256.New, []byte(b.ApiSecret))
-	io.WriteString(h, query)
-	query += fmt.Sprintf("&sign=%x", h.Sum(nil))
+func (b *Bybit) get(path string, params url.Values, result interface{}) error {
+	query := params.Encode()
+	timestamp := b.timestamp()
 
 	u := url.URL{
 		Scheme:   "https",
@@ -66,13 +44,22 @@ func (b *Bybit) signedUrl(path string, addParams map[string]string) string {
 		Path:     path,
 		RawQuery: query}
 
-	return u.String()
-}
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return err
+	}
 
-func (b *Bybit) get(path string, params map[string]string, result interface{}) error {
-	u := b.signedUrl(path, params)
+	h := hmac.New(sha256.New, []byte(b.ApiSecret))
+	io.WriteString(h, timestamp+b.ApiKey+query)
+	signature := fmt.Sprintf("%x", h.Sum(nil))
 
-	resp, err := http.Get(u)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-BAPI-API-KEY", b.ApiKey)
+	req.Header.Set("X-BAPI-SIGN", signature)
+	req.Header.Set("X-BAPI-TIMESTAMP", timestamp)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
 		return err
 	}
@@ -99,7 +86,7 @@ func (b *Bybit) GetFundingRate() ([2]float64, error) {
 	var response fundingResponse
 
 	err = b.get("/v5/market/tickers",
-		map[string]string{"category": "inverse", "symbol": "BTCUSD"}, &response)
+		url.Values{"category": {"inverse"}, "symbol": {"BTCUSD"}}, &response)
 	if err != nil {
 		return [2]float64{}, err
 	}
@@ -114,38 +101,63 @@ func (b *Bybit) GetFundingRate() ([2]float64, error) {
 	return [2]float64{funding, 0}, nil
 }
 
-func (b *Bybit) GetSize() int {
+func (b *Bybit) positionRequest() (positionResponse, error) {
 	var response positionResponse
 
-	err := b.get("/v2/private/position/list",
-		map[string]string{"symbol": "BTCUSD"}, &response)
+	err := b.get("/v5/position/list",
+		url.Values{"category": {"inverse"}, "symbol": {"BTCUSD"}}, &response)
 
+	if err != nil {
+		return response, err
+	}
+
+	if len(response.Result.List) != 1 {
+		return response, errors.New("Unexpected position response")
+	}
+
+	return response, nil
+}
+
+func (b *Bybit) GetSize() int {
+	response, err := b.positionRequest()
 	if err != nil {
 		return 0
 	}
 
-	return response.Result.Size
+	size, _ := strconv.Atoi(response.Result.List[0].Size)
+	return size
 }
 
 func (b *Bybit) GetEquityAndLeverage() ([2]float64, error) {
-	var response positionResponse
+	var response walletResponse
 
-	err := b.get("/v2/private/position/list",
-		map[string]string{"symbol": "BTCUSD"}, &response)
+	err := b.get("/v5/account/wallet-balance",
+		url.Values{"accountType": {"CONTRACT"}, "coin": {"BTC"}}, &response)
 
 	if err != nil {
-		log.WithField("venue", "bybit").Warn(err.Error())
-		return [2]float64{0, 0}, err
+		return [2]float64{}, err
 	}
 
-	walletBalance, _ := strconv.ParseFloat(response.Result.WalletBalance, 64)
-	positionValue, _ := strconv.ParseFloat(response.Result.PositionValue, 64)
+	if len(response.Result.List) != 1 {
+		return [2]float64{}, errors.New("Unexpected wallet response")
+	}
 
-	equity := walletBalance + response.Result.UnrealisedPnl
+	if len(response.Result.List[0].Coin) != 1 {
+		return [2]float64{}, errors.New("Unexpected coin response")
+	}
 
+	equity, _ := strconv.ParseFloat(response.Result.List[0].Coin[0].Equity, 64)
 	if equity == 0 {
-		return [2]float64{0, 0}, nil
+		return [2]float64{}, nil
 	}
+
+	resp, err := b.positionRequest()
+	if err != nil {
+		log.WithField("venue", "bybit").Warn(err.Error())
+		return [2]float64{equity, 0}, nil
+	}
+
+	positionValue, _ := strconv.ParseFloat(resp.Result.List[0].PositionValue, 64)
 
 	return [2]float64{equity, (positionValue / equity)}, nil
 }
--- bybit/structs.go
+++ bybit/structs.go
@@ -10,9 +10,19 @@ type fundingResponse struct {
 
 type positionResponse struct {
 	Result struct {
-		Size          int     `json:"size"`
-		PositionValue string  `json:"position_value"`
-		WalletBalance string  `json:"wallet_balance"`
-		UnrealisedPnl float64 `json:"unrealised_pnl"`
+		List []struct {
+			Size          string `json:"size"`
+			PositionValue string `json:"positionValue"`
+		} `json:"list"`
+	} `json:"result"`
+}
+
+type walletResponse struct {
+	Result struct {
+		List []struct {
+			Coin []struct {
+				Equity string `json:"equity"`
+			} `json:"coin"`
+		} `json:"list"`
 	} `json:"result"`
 }
